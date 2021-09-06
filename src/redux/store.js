c1c03e6f0d4ab71a682dbf415c3f4ff4678d40ce
hugues0 <ntwari.hugues@gmail.com>
2021-09-06 13:59:21 +0200

    setting up redux store

---
+++ src/redux/store.js
@@ -0,0 +1,14 @@
+import {createStore,applyMiddleware} from 'redux'
+import logger from "redux-logger"
+import reduxThunk from 'redux-thunk'
+import rootReducer from '/root-reducer'
+
+const middlwewares = [reduxThunk]
+
+if (process.env.NODE_ENV === "development") {
+    middlwewares.push(logger);
+}
+
+const store = createStore(rootReducer,applyMiddleware(...middlwewares))
+
+export default store 
